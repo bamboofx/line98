cdf07dd02c25cb73a332f4434e0ef422e2c2a28a
tao <tiger.crystal.vn@gmail.com>
2026-03-02 09:30:59 +0700

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1156459901"
+    serviceWorkerVersion: "341086383"
   }
 });
